e6a7a1be30874cdd7dbc5527d0aa2746d8bc2016
slevinger <shir.levinger@gmail.com>
2020-08-25 20:31:36 +0300

    Updates

---
+++ precache-manifest.85a2db4c2aae6895973102444270c24f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d51403b2975d5109cdd07b4f86531e5",
+    "url": "/scales/index.html"
+  },
+  {
+    "revision": "ed7d95d710466f5688bd",
+    "url": "/scales/static/css/main.4ca2d80f.chunk.css"
+  },
+  {
+    "revision": "75613210662739313d76",
+    "url": "/scales/static/js/2.51270a11.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/scales/static/js/2.51270a11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed7d95d710466f5688bd",
+    "url": "/scales/static/js/main.5132779a.chunk.js"
+  },
+  {
+    "revision": "143ddb95dda539802b56",
+    "url": "/scales/static/js/runtime-main.f3cc92f2.js"
+  },
+  {
+    "revision": "9c46095118380d38f12e67c916b427f9",
+    "url": "/scales/static/media/Montserrat.9c460951.ttf"
+  }
+]);
